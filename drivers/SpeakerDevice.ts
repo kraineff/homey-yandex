aa621b308131dd86da35375a89627239b4f90082
Alexey Krainev <xmrvizzy@ya.ru>
2022-03-10 03:09:24 +0500

    Оптимизировал класс колонки

--- drivers/SpeakerDevice.ts
+++ drivers/SpeakerDevice.ts
@@ -26,26 +26,14 @@ export default class SpeakerDevice extends Homey.Device {
         this.image = await this.app.homey.images.createImage();
         await this.setAlbumArtImage(this.image);
 
-        await this.onDataListener();
-        await this.onMultipleCapabilityListener();
-
         if (this.yandex.ready) await this.init();
         else await this.setUnavailable(this.homey.__("device.reauth_required"));
 
-        this.yandex.on("ready", async () => {
-            await this.init();
-        });
-
+        this.yandex.on("update", async data => await this.setSettings({ x_token: data.x_token, cookies: data.cookies }));
+        this.yandex.on("ready", async () => await this.init());
         this.yandex.on("reauth_required", async () => {
             await this.setUnavailable(this.homey.__("device.reauth_required"));
             this.glagol.close();
-        });
-
-        this.yandex.on("update", async data => {
-            await this.setSettings({
-                x_token: data.x_token,
-                cookies: data.cookies
-            });
         });
     }
 
@@ -53,40 +41,19 @@ export default class SpeakerDevice extends Homey.Device {
         await this.setAvailable();
         
         this.speaker = this.yandex.devices.getSpeaker(this.getData()["id"])!;
-
+        await this.onMultipleCapabilityListener();
         await this.initSettings();
-        await this.checkLocalConnection();
-    }
-
-    async initSettings() {
-        if (["yandexstation_2", "yandexmini_2"].includes(this.driver.id)) {
-            let config = await this.yandex.devices.getSpeakerConfig(this.speaker);
-            if (config.led) {
-                await this.setSettings({
-                    auto_brightness: config.led.brightness.auto,
-                    brightness: config.led.brightness.value,
-                    music_equalizer_visualization: config.led.music_equalizer_visualization.auto ? "auto" : config.led.music_equalizer_visualization.style,
-                    time_visualization: config.led.time_visualization.size
-                });
-            }
-        }
-        
-        await this.setSettings({
-            x_token: this.homey.settings.get("x_token"),
-            cookies: this.homey.settings.get("cookies")
-        });
+        await this.localConnection();
     }
     
-    async checkLocalConnection() {
+    async localConnection() {
         if (this.getData()["local_id"]) {
             const localUrl = () => `wss://${this.getStoreValue("address")}:${this.getStoreValue("port")}`;
             const updateData = (address: string, port: string) => {
                 this.setStoreValue("address", address);
                 this.setStoreValue("port", port);
             }
-            await this.glagol.init(this.speaker, localUrl).then(() => {
-                this.isLocal = true;
-            });
+            await this.glagol.init(this.speaker, localUrl).then(() => this.isLocal = true);
 
             try {
                 let discoveryResult = <DiscoveryResultMDNSSD>this.app.discoveryStrategy.getDiscoveryResult(this.getData()["local_id"]);
@@ -96,39 +63,37 @@ export default class SpeakerDevice extends Homey.Device {
             this.app.discoveryStrategy.on("result", async (discoveryResult: DiscoveryResultMDNSSD) => {
                 if (discoveryResult.id === this.getData()["local_id"]) updateData(discoveryResult.address, discoveryResult.port);
             });
+
+            this.glagol.on(this.getData()["id"], async state => {
+                delete state.timeSinceLastVoiceActivity;
+                delete state.playerState.duration;
+                delete state.playerState.progress;
+                const difference: any = diff(this.lastState, state);
+                if (!Object.keys(difference).length) return;
+                this.lastState = state;
+    
+                if (this.lastState.aliceState === "LISTENING" && this.waitForIdle) {
+                    if (this.savedVolumeLevel !== undefined) this.glagol.send({ command: "setVolume", volume: this.savedVolumeLevel });
+                    this.waitForIdle = false;
+                }
+    
+                if (difference.volume !== undefined) await this.setCapabilityValue("volume_set", difference.volume * 10);
+                if (difference.playing !== undefined) await this.setCapabilityValue("speaker_playing", difference.playing);
+                if (difference.playerState?.subtitle !== undefined) await this.setCapabilityValue("speaker_artist", difference.playerState.subtitle);
+                if (difference.playerState?.title !== undefined) await this.setCapabilityValue("speaker_track", difference.playerState.title);
+                if (difference.playerState?.duration !== undefined) await this.setCapabilityValue("speaker_duration", difference.playerState.duration);
+                if (difference.playerState?.progress !== undefined) await this.setCapabilityValue("speaker_position", difference.playerState.progress);
+                if (difference.playerState?.extra?.coverURI !== undefined) {
+                    this.image.setUrl(`https://${(<string>difference.playerState.extra.coverURI).replace("%%", "400x400")}`);
+                    await this.image.update();
+                }
+            });
         }
     }
 
-    // При получении данных
-    async onDataListener() {
-        this.glagol.on(this.getData()["id"], async state => {
-            delete state.timeSinceLastVoiceActivity;
-            delete state.playerState.duration;
-            delete state.playerState.progress;
-            const difference: any = diff(this.lastState, state);
-            if (!Object.keys(difference).length) return;
-            this.lastState = state;
-
-            if (this.lastState.aliceState === "LISTENING" && this.waitForIdle) {
-                if (this.savedVolumeLevel !== undefined) this.glagol.send({ command: "setVolume", volume: this.savedVolumeLevel });
-                this.waitForIdle = false;
-            }
-
-            if (difference.volume !== undefined) await this.setCapabilityValue("volume_set", difference.volume * 10);
-            if (difference.playing !== undefined) await this.setCapabilityValue("speaker_playing", difference.playing);
-            if (difference.playerState?.subtitle !== undefined) await this.setCapabilityValue("speaker_artist", difference.playerState.subtitle);
-            if (difference.playerState?.title !== undefined) await this.setCapabilityValue("speaker_track", difference.playerState.title);
-            if (difference.playerState?.duration !== undefined) await this.setCapabilityValue("speaker_duration", difference.playerState.duration);
-            if (difference.playerState?.progress !== undefined) await this.setCapabilityValue("speaker_position", difference.playerState.progress);
-            if (difference.playerState?.extra?.coverURI !== undefined) {
-                this.image.setUrl(`https://${(<string>difference.playerState.extra.coverURI).replace("%%", "400x400")}`);
-                await this.image.update();
-            }
-        });
-    }
-
-    // При изменении данных
     async onMultipleCapabilityListener() {
+        this.registerCapabilityListener("button.reauth", () => { this.yandex.emit("reauth_required") });
+        
         this.registerCapabilityListener("volume_set", async (value) => {
             if (!this.isLocal) await this.yandex.scenarios.send(this.speaker, `громкость на ${value / 10}`);
             else this.glagol.send({ command: "setVolume", volume: value / 10 });
@@ -147,7 +112,6 @@ export default class SpeakerDevice extends Homey.Device {
                 if (volume >= 0) this.glagol.send({ command: "setVolume", volume: volume });
             }
         });
-
         this.registerCapabilityListener("speaker_playing", async (value) => {
             if (!this.isLocal) await this.yandex.scenarios.send(this.speaker, value ? "продолжить" : "пауза");
             else this.glagol.send({ command: value ? "play" : "stop" });
@@ -160,17 +124,26 @@ export default class SpeakerDevice extends Homey.Device {
             if (!this.isLocal) await this.yandex.scenarios.send(this.speaker, "прошлый трек");
             else this.glagol.send({ command: "prev" });
         });
-
-        this.registerCapabilityListener("button.reauth", () => { this.yandex.emit("reauth_required") });
     }
 
-    // При удалении устройства
-    async onDeleted(): Promise<void> {
-        this.glagol.close();
+    // Настройки
+    async initSettings() {
+        await this.setSettings({ x_token: this.homey.settings.get("x_token"), cookies: this.homey.settings.get("cookies") });
+        
+        if (["yandexstation_2", "yandexmini_2"].includes(this.driver.id)) {
+            let config = await this.yandex.devices.getSpeakerConfig(this.speaker);
+            if (config.led) {
+                await this.setSettings({
+                    auto_brightness: config.led.brightness.auto,
+                    brightness: config.led.brightness.value,
+                    music_equalizer_visualization: config.led.music_equalizer_visualization.auto ? "auto" : config.led.music_equalizer_visualization.style,
+                    time_visualization: config.led.time_visualization.size
+                });
+            }
+        }
     }
 
-    // При изменении настроек
-    async onSettings({ oldSettings, newSettings, changedKeys }: { oldSettings: any; newSettings: any; changedKeys: string[]; }): Promise<string | void> {
+    async onSettings({ newSettings, changedKeys }: { oldSettings: any; newSettings: any; changedKeys: string[]; }): Promise<string | void> {
         if (this.yandex.ready) {
             if (["yandexstation_2", "yandexmini_2"].includes(this.driver.id)) {
                 const config = await this.yandex.devices.getSpeakerConfig(this.speaker);
@@ -193,5 +166,10 @@ export default class SpeakerDevice extends Homey.Device {
             }
             return this.homey.__("device.save_settings");
         }
+    }
+
+    // Остальное
+    async onDeleted(): Promise<void> {
+        this.glagol.close();
     }
 }
